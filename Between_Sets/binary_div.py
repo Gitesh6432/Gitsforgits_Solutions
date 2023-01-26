ca6ca196f612acf1549fbfee1ebe5fce6429f034
truly_gits <giteshchaudhari6432@gmail.com>
2023-01-26 23:37:42 +0530

    Add files via upload

---
+++ Between_Sets/binary_div.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    # Write your code here
+    import itertools as it
+    ff=0
+    for x in range(max(a),b[0]+1):
+        la1=[1 for z in a if x%z==0]
+        la2=[1 for d in b if d%x==0]
+        if len(la1)==len(a) and len(la2)==len(b):
+            ff+=1
+    return ff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
